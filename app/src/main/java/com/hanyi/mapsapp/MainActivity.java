4785d4f9fd3cdd8601f67ac637c07c64ccd7e33c
Hanyi Chen <chanhanyi0923@gmail.com>
2019-11-14 17:08:19 +0800

    init

---
+++ app/src/main/java/com/hanyi/mapsapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.hanyi.mapsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
