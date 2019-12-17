4442f0a9ad75b6e223129acacdac094d597a4f93
Oscarshu0719 <vm3y3rmp40719@gmail.com>
2019-12-17 21:08:48 +0800

    Fix minor bugs

--- app/src/main/java/com/hanyi/mapsapp/LoginActivity.java
+++ app/src/main/java/com/hanyi/mapsapp/LoginActivity.java
@@ -1,7 +1,5 @@
 package com.hanyi.mapsapp;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
